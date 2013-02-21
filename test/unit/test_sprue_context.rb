57481313dc57775686cb534fc34cb3d45a09f719
Scott Tadman <github@tadman.ca>
2013-02-21 11:54:35 -0500

    Queues are no longer persisted by default, used as a handle instead

--- test/unit/test_sprue_context.rb
+++ test/unit/test_sprue_context.rb
@@ -46,7 +46,7 @@ class TestSprueContext < Test::Unit::TestCase
 
     assert_equal 'default', queue.ident
 
-    assert_equal true, context.repository.entity_exist?(queue)
+    assert_equal false, context.repository.entity_exist?(queue)
     assert_equal 0, queue.length
   end
 end
